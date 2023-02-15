37ead81b1035dd31b02a4935fcb8ff6289495659
jouvychen <2774721329@qq.com>
2023-02-15 14:58:13 +0800

    #feat：配置打包路径为相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,10 @@ export default defineConfig({
     imports: ['vue', 'vue-router'],
     // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
     dts: 'src/auto-import.d.ts',
-  }),], resolve: {
+  })], resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  base: './',
 })
