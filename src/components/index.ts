b45ac1539df5a9847974a9256cba1bed13b69b31
ChenJW <2774721329@qq.com>
2024-02-21 13:32:12 +0800

    feat: 全局注册播放和暂停css按钮

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 import GridLayout from './grid-layout.vue'; // 卡片布局
+import CssPlayPauseButton from './css-play-pause-button.vue'; // css播放暂停按钮
 
 const commonComponent = {
   install(Vue: any) {
     Vue.component('GridLayout', GridLayout);
+    Vue.component('CssPlayPauseButton', CssPlayPauseButton);
   },
 };
 export default commonComponent;
